777b64886a97f263952dde003c2d078ba4c2931f
xtadeushx <alekscherkasov13@gmail.com>
2023-01-30 18:07:34 +0200

    feat: add accordion component

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,7 @@
 // import Swiper, { Navigation, Pagination } from 'swiper';
-import {burgerMenu} from './header.js'
-console.log('Hello from')
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    burgerMenu()
-    console.log('Loaded')
+   
+    console.log('Loaded');
 });
